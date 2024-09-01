332421aaf54a474ba0f9fa22f67b9b4435f4902e
NikVogri <vogrinec.nik@gmail.com>
2024-09-01 16:32:45 +0200

    fix: reserved env variables

--- src/core/s3.ts
+++ src/core/s3.ts
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
 	region: process.env.AWS_S3_REGION,
-	credentials: { accessKeyId: process.env.AWS_ACCESS_KEY!, secretAccessKey: process.env.AWS_SECRET_KEY! },
+	credentials: { accessKeyId: process.env.AWS_USER_ACCESS_KEY!, secretAccessKey: process.env.AWS_USER_SECRET_KEY! },
 });
 
 export default s3;
