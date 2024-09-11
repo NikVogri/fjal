c646a7442a5a700ada3a6043c1f8c9bcc4d87695
NikVogri <vogrinec.nik@gmail.com>
2024-09-11 20:22:35 +0200

    fix: deploy by setting git config core.ignorecase to false

---
+++ src/app/components/UI/card.tsx
@@ -0,0 +1,7 @@
+export default function Card({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="py-8 px-4 rounded-2xl w-96 min-h-32 md:shadow-xl md:border-2 border-solid border-gray-300/30">
+			{children}
+		</div>
+	);
+}
