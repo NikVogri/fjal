c040c9ad253b4b7692f676a276359a7adf962dda
Nik Vogrinec <55229210+NikVogri@users.noreply.github.com>
2024-09-11 20:29:26 +0200

    fix: deploy renamed component is still commited for some reason

--- src/app/components/UI/Card.tsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }: { children: React.ReactNode }) {
-	return (
-		<div className="py-8 px-4 rounded-2xl w-96 min-h-32 md:shadow-xl md:border-2 border-solid border-gray-300/30">
-			{children}
-		</div>
-	);
-}
