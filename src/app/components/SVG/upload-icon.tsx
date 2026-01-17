d50437c3522853d03bdfaa2926b02fe3d07741e8
NikVogri <vogrinec.nik@gmail.com>
2026-01-17 23:05:05 +0100

    feat: add dropzone to file upload form

--- src/app/components/SVG/upload-icon.tsx
+++ src/app/components/SVG/upload-icon.tsx
@@ -1,11 +1,11 @@
-export const UploadIcon = () => {
+export const UploadIcon = ({ color }: { color?: string }) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
 			strokeWidth={1.5}
-			stroke="currentColor"
+			stroke={color ? color : "currentColor"}
 			className="size-6"
 		>
 			<path
